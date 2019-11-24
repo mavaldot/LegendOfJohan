08708add5d326bc1fe0083bb2eea035357d5d51d
mavaldot <mavaldot@gmail.com>
2019-11-24 11:15:31 -0500

    first commit

---
+++ src/controller/GameController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class GameController {
+
+	public void begin() {
+		
+	}
+	
+}
---
+++ src/controller/MenuController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MenuController {
+	
+	@FXML
+	private Button start;
+	
+	public void startGame(ActionEvent e) throws IOException {
+		
+		Scene menuScene = ((Node) e.getSource()).getScene();
+		
+		FXMLLoader gameLoader = new FXMLLoader(getClass().getResource("/view/Game.fxml"));
+		Parent gamePane = gameLoader.load();
+		
+		Stage stage = (Stage) menuScene.getWindow();
+		Scene gameScene = new Scene(gamePane, 800, 600);
+		GameController gameController = (GameController) gameLoader.getController();
+		gameController.begin();
+		stage.setScene(gameScene);
+		
+		
+		
+	}
+	
+}
